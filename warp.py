2d4415cc1348ee17da60407a6529b879c298fce1
Kostas Niktas <kostasniktas@users.noreply.github.com>
2022-03-25 19:55:01 -0700

    prep for synced core

--- warp.py
+++ warp.py
@@ -4,8 +4,11 @@ import time
 import subprocess
 
 NUM_PIXELS = 16
-MIDDLE = 6
-BOTTOM_FIRST = (NUM_PIXELS / 2) >= MIDDLE
+
+# Assume middle is right in the middle or in the bottom half
+MIDDLE = 6 # location of core
+# BOTTOM_SHORTER = (NUM_PIXELS / 2) >= MIDDLE
+BOTTOM_SHORTER = True
 
 
 def get_pixels() -> List[Tuple[int,int,int]]:
@@ -27,24 +30,23 @@ def warp_pixels() -> None:
     pixels = get_pixels()
     last_top = NUM_PIXELS
     last_bottom = -1
+    diff = NUM_PIXELS - MIDDLE + 1
+    # print(diff)
+    # print(NUM_PIXELS - MIDDLE + 1)
+    # sys.exit(0)
     while True:
         new_bottom = last_bottom + 1
         new_top = last_top - 1
 
         # Resetting
-        if BOTTOM_FIRST:
-            if new_bottom > MIDDLE:
-                new_bottom = 0
-            if new_top <= MIDDLE:
-                new_top = NUM_PIXELS - 1
-        else:
-            if new_top < MIDDLE:
-                new_top = NUM_PIXELS - 1
-            if new_bottom <= MIDDLE:
-                new_bottom = 0
+        # assumes core at middle or lower
+        if new_bottom > MIDDLE:
+            new_bottom = 0
+        if new_top <= MIDDLE:
+            new_top = NUM_PIXELS - 1
 
         # Colors
-        pixels = get_pixels()
+        # pixels = get_pixels()
         pixels[new_top] = (0,0,255)
         pixels[new_bottom] = (0,0,255)
         if last_bottom > -1:
@@ -55,6 +57,15 @@ def warp_pixels() -> None:
         # View
         subprocess.call("clear")
         print_pixels(pixels)
+
+        try:
+            pixels[new_top] = (0,0,0)
+            pixels[new_bottom] = (0,0,0)
+            pixels[last_top] = (0,0,0)
+            pixels[last_bottom] = (0,0,0)
+        except IndexError:
+            pass
+
         last_top = new_top
         last_bottom = new_bottom
         time.sleep(1)
